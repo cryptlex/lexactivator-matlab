f298c229e88e299932198b5e1f9ff92730b022b5
muneebkq <muneebkq05@gmail.com>
2024-09-06 17:25:05 +0530

    refactor: remove changes

--- examples/Sample.m
+++ examples/Sample.m
@@ -1,15 +1,17 @@
-sProductData = 'MjJEODM4OTQwNTUzMzYxNjM5NDk4MDI4OTZERDY0NkQ=.fcuUtjVJZ+zZ+E8SzqFoWjdk4RgojBmfxX7CVzWNJOVBSfGS8/XurBvnPsOEquFB80uNIAcqixhxnAlP9TGU1fmPFA1znSV5PAk/uZ3y2Bga636zqWcbCjcPZhJ2k50HBhsFdZO3+eqA/4AdPW+tNgaZ4FMZByy+n4HTkBC03yWQFdGLz9knFyier2Z9JJzE0ufSU/p5m8Mh2XZGa1rb3gOUmxGRa4GMJgHrVfGOEyGR/LKfObomxCwoML8NCUVWzjs308KuoxSYpj9UusrKC5stRxuOZZBtf7Oq0+khrdLu20mdP9UVR370RIF2phS/Y/XownlN+eIkMfzkoeCEGx7bQZSYw4HKvYYlmLQNymYDer4wG9Tqj2hkBhTzCxSBrWAzquc+CRlQp+Wc3dj2jIdMYEAO1RS+VC82FSZxGCcwyXtkBaiOEsuRfBPUl0A9DSIp6eNmol7NpXwkyUsavWmo04uday+WaDsntPSO3P3BMXG8aaCGELJibHyA2ZjWPpdG+3da5FrutGy9C4vWIx2gA2sjmjSYzP5wKNgzh41D4MroKBwGUZhO3Li6YbDJxkOqDaoryd7TKdsc36QkNe8z8QPRbXsO3Xv4yCOcb2d3dKiYfGwQyEG4m/1g8EEvDC3+AgUAerbJpH9EGEoQAqMpmIdm1pJkO97DMT7DQdlYqLiDVpjqDL0+VHPKy4EBPoeskcUeNae3FVOg1XDh7QKIvHXkVmu9gewRxHQiUQc0vgUrtC+EAdxb7s/peBoNonAOHaMUsWlmz5HLqlbHNOWtyD8HqTbTUVQoZtuvM3HVhVde2mQ2l4kiRM0Q1JTL';
-sProductId = 'de5c962f-75b0-417d-a72a-5b474b8cb4bb';
+sProductData = 'PASTE_CONTENT_OF_PRODUCT.DAT_FILE';
+sProductId = 'PASTE_PRODUCT_ID';
 sReleaseVersion = '1.0.0';
 
 %Loads the C library of LexActivator
 sHeaderFile = './LexActivator.h';
 sStatusHeaderFile = './LexStatusCodes.h';
 sSharedLibrary = 'LexActivator';
-
+%unloadlibrary(sSharedLibrary)
 if not(libisloaded(sSharedLibrary))
    loadlibrary(sSharedLibrary,sHeaderFile, 'addheader',sStatusHeaderFile);
 end
+%Creates and prints the list of the functions of the library with their arguments
+%list = libfunctions(sSharedLibrary,'-full');
 
 % Calls function to set the product data
 status = calllib(sSharedLibrary,'SetProductData',toString(sProductData));
@@ -18,14 +20,12 @@ if status ~= 0
     fprintf('Error Code: %.0f\n',status)
     return
 end
-
 % Calls function to set the product id
 status = calllib(sSharedLibrary,'SetProductId',toString(sProductId), uint32(1));
 if status ~= 0
     fprintf('Error Code: %.0f\n',status)
     return
 end
-
 % Calls function to set the release version
 status = calllib(sSharedLibrary,'SetReleaseVersion',toString(sReleaseVersion));
 if status ~= 0
@@ -37,23 +37,26 @@ end
 status = calllib(sSharedLibrary,'IsLicenseGenuine');
 if status == 0
     fprintf('License is genuinely activated!\n')
+    % [status, metadataKey, metadataValue] = calllib(sSharedLibrary,'GetLicenseMetadata', toString('feature1'), blanks(256),256 );
+    % disp(metadataValue);
 elseif status == 20
     fprintf('License is genuinely activated but has expired!\n')
-    getLicenseAllowedDeactivations();  % <-- Explicit call added here
 elseif status == 21
     fprintf('License is genuinely activated but has been suspended!\n')
 elseif status == 22
     fprintf('License is genuinely activated but grace period is over!\n')
 else
-    % Calls function to check if trial is genuine
+    % Calls function to check if license is activated
     trialStatus = calllib(sSharedLibrary,'IsTrialGenuine');
     if trialStatus == 0
-        fprintf('Trial is valid\n')
+        fprintf('Trial is valid')
     elseif trialStatus == 25
-        fprintf('Trial has expired!\n')
+        fprintf('Trial has expired!')
+        % Time to buy the license and activate the app
         activate()
     else
-        fprintf('Either trial has not started or has been tampered with!\n')
+        fprintf('Either trial has not started or has been tampered!\n')
+        % Activating the trial
         activateTrial()
     end
 end
@@ -68,15 +71,13 @@ end
 
 function activate()
     sSharedLibrary = 'LexActivator';
-    sLicenseKey = '064DA0-12BB7F-4EA489-F13C48-742ACE-FFFB2C';
-    
+    sLicenseKey = 'PASTE_LICENCE_KEY';
     % Calls function to set the license key
     status = calllib(sSharedLibrary,'SetLicenseKey', toString(sLicenseKey));
 	if status ~= 0
 		fprintf('Error Code: %.0f\n',status)
 		return
 	end
-    
     % Calls function to activate the license key
     status = calllib(sSharedLibrary,'ActivateLicense');
 	if status == 0 || status == 20 || status == 21
@@ -86,22 +87,8 @@ function activate()
 	end
 end
 
-function getLicenseAllowedDeactivations()
-    sSharedLibrary = 'LexActivator';
-    
-    % Calls function to get license allowed deactivations
-    [status, allowedDeactivations] = calllib(sSharedLibrary, 'GetLicenseAllowedDeactivations', int32(0));
-    
-    if status == 0
-        fprintf('Allowed Deactivations: %d\n', allowedDeactivations);
-    else
-        fprintf('Failed to get allowed deactivations, Status: %d\n', status);
-    end
-end
-
 function activateTrial()
     sSharedLibrary = 'LexActivator';
-    
     % Calls function to activate the trial
     status = calllib(sSharedLibrary,'ActivateTrial');
 	if status == 0
@@ -109,6 +96,6 @@ function activateTrial()
 	elseif status == 25
 		fprintf('Product trial has expired!\n')
 	else
-		fprintf('Product trial activation failed: %.0f\n',status)
+		fprintf('Product trial activation failed:%.0f\n',status)
 	end
 end
